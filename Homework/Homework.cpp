f76b9439ff838f832e37020a62af0fa9c2b0da69
admin1 <admin1@ADMIN1-PC36>
2022-07-22 17:58:02 +0300

    Добавьте файлы проекта.

---
+++ Homework/Homework.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string> 
+using namespace std;
+int main() {
+    bool flag = false, flag2 = false;
+    ofstream file;
+    file.open("File.txt");
+    file.clear();
+
+    for (size_t i = 1955; i <= 2022; i++)
+    {
+        if (i >= 2022 && flag2) continue;
+
+        for (size_t j = 1; j <= 12; j++)
+        {
+            if (i >= 2022 && j >= 6 && flag) {
+                flag2 = true;
+                continue;
+            }
+
+            for (size_t k = 1; k <= 30; k++)
+            {
+                if (i >= 2022 && j >= 6 && k >= 22) {
+                    flag = true;
+                    continue;
+                }
+                file << i;
+                if (j < 10) file << 0;
+                file << j;
+                if (k < 10) file << 0;
+                file << k << endl;
+
+            }
+        }
+    }
+    file.close();
+}
